a2632a1543c927554ee91c36c23c4f97a701b706
ErPravinYadav <pravinyadav8463@gmail.com>
2022-10-25 16:21:03 +0530

    commit first by Pravin

---
+++ eclipse-workspace/ATT/src/test/java/scripts/Demo.java
@@ -0,0 +1,10 @@
+package scripts;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is demo");
+	}
+
+}
---
+++ eclipse-workspace/ATT/src/test/java/scripts/Semple.java
@@ -0,0 +1,9 @@
+package scripts;
+
+public class Semple {
+
+	public static void main(String[] args) {
+System.out.println("this is semple");
+	}
+
+}
